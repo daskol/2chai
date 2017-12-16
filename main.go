bbf7fbad325c59435b4668401e57c4cbaca3b6fc
Daniel Bershatsky <daniel.bershatsky@skolkovotech.ru>
2017-12-17 00:54:33 +0300

    Synchonize board with threads completly.

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"database/sql"
 	"flag"
-	"log"
 	"os"
 	"strconv"
 	"time"
@@ -13,6 +12,8 @@ import (
 	"github.com/daskol/2chai/api"
 	"github.com/google/subcommands"
 	_ "github.com/lib/pq"
+	log "github.com/sirupsen/logrus"
+	"golang.org/x/sync/errgroup"
 )
 
 var dsn string
@@ -128,6 +129,162 @@ func (l *listPosts) Execute(_ context.Context, f *flag.FlagSet, _ ...interface{}
 	return subcommands.ExitSuccess
 }
 
+// syncAll реализует интерфейс subcommands.Commander для того, чтобы
+// синхронизировать базу постов с некоторой переодичностью.
+type syncAll struct{}
+
+func (s *syncAll) Name() string     { return "sync-all" }
+func (s *syncAll) Synopsis() string { return "Synchronize all in background." }
+func (s *syncAll) Usage() string {
+	return "sync-all\n"
+}
+
+func (s *syncAll) SetFlags(_ *flag.FlagSet) {}
+
+func (s *syncAll) Execute(_ context.Context, f *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
+	if len(f.Args()) > 0 {
+		log.Println("Too many arguments.")
+		return subcommands.ExitUsageError
+	}
+
+	log.Println("connect to database")
+	db, err := sql.Open("postgres", dsn)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("enter in synchronization loop")
+	duration := 1200 * time.Second
+	timer := time.NewTimer(0)
+
+	defer timer.Stop()
+
+	for range timer.C {
+		timer.Reset(duration)
+		go func() {
+			if err := s.syncAll(db); err != nil {
+				log.Errorf("%s: skip iteration", err)
+			} else {
+				log.Info("done.")
+			}
+		}()
+	}
+
+	return subcommands.ExitSuccess
+}
+
+func (s *syncAll) syncAll(db *sql.DB) error {
+	log.Println("query list of known boards from database")
+	boardIDs, boardAbbrs, err := s.listBoards(db)
+
+	if err != nil {
+		return err
+	}
+
+	ch := make(chan int, len(boardIDs))
+
+	log.Println("synchronize all threads in all boards")
+	for i := range boardIDs {
+		ch <- i
+	}
+
+	worker := func() error {
+		for i := range ch {
+			boardID := boardIDs[i]
+			boardAbbr := boardAbbrs[i]
+
+			log.Infof("%03d request thread catalog of board %s",
+				i, boardAbbr)
+
+			if err := s.syncBoard(boardID, boardAbbr, db); err != nil {
+				return err
+			}
+		}
+
+		return nil
+	}
+
+	grp := errgroup.Group{}
+	grp.Go(worker)
+	grp.Go(worker)
+	grp.Go(worker)
+
+	close(ch)
+
+	return grp.Wait()
+}
+
+func (s *syncAll) listBoards(db *sql.DB) ([]int, []string, error) {
+	rows, err := sq.
+		Select("board_id", "abbr").
+		From("boards").
+		Where(sq.Eq{"watch": true}).
+		PlaceholderFormat(sq.Dollar).
+		RunWith(db).
+		Query()
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	defer rows.Close()
+	boardIDs := []int{}
+	boardAbbrs := []string{}
+
+	for rows.Next() {
+		boardID := 0
+		boardAbbr := ""
+
+		if err := rows.Scan(&boardID, &boardAbbr); err != nil {
+			return nil, nil, err
+		}
+
+		boardIDs = append(boardIDs, boardID)
+		boardAbbrs = append(boardAbbrs, boardAbbr)
+	}
+
+	return boardIDs, boardAbbrs, nil
+}
+
+func (s *syncAll) syncBoard(boardID int, boardAbbr string, db *sql.DB) error {
+	threads, err := api.ListThreadCatalog(boardAbbr)
+
+	if err != nil {
+		return err
+	}
+
+	if err := upsertThreads(boardID, threads, db); err != nil {
+		return err
+	}
+
+	for _, thread := range threads.Threads {
+		threadID := thread.Num
+
+		if err := s.syncThread(boardAbbr, boardID, threadID, db); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (s *syncAll) syncThread(boardAbbr string, boardID, threadID int, db *sql.DB) error {
+	posts, err := api.ListPosts(boardAbbr, strconv.Itoa(threadID))
+
+	if err != nil {
+		return err
+	}
+
+	return upsertPosts(boardID, threadID, posts, db)
+}
+
 // syncBoards реализует интерфейс subcommands.Commander для того, чтобы
 // наполнить базу данных списком доступных досок.
 type syncBoards struct{}
@@ -226,6 +383,8 @@ func (s *syncPosts) Execute(_ context.Context, f *flag.FlagSet, _ ...interface{}
 		log.Fatal(err)
 	} else if err := s.upsertPosts(board, thread, lst); err != nil {
 		log.Fatal(err)
+	} else {
+		log.Info("done.")
 	}
 
 	return subcommands.ExitSuccess
@@ -260,48 +419,7 @@ func (s *syncPosts) upsertPosts(board, thread string, posts []*api.Post) error {
 		return err
 	}
 
-	log.Println("prepare insert or update statement")
-	stmt := sq.
-		Insert("posts").
-		Columns("post_id", "thread_id", "board_id",
-			"ordinal", "op",
-			"author", "email", "subject", "comment",
-			"created_at").
-		Suffix("ON CONFLICT (board_id, post_id) DO NOTHING").
-		PlaceholderFormat(sq.Dollar).
-		RunWith(db)
-
-	for _, post := range posts {
-		createdAt := time.Unix(post.Timestamp, 0)
-
-		stmt = stmt.Values(post.Num, threadID, boardID,
-			post.Number, post.Op,
-			post.Name, post.Email, post.Subject, post.Comment,
-			createdAt)
-	}
-
-	log.Println("execute statement")
-
-	if _, err := stmt.Exec(); err != nil {
-		return err
-	}
-
-	log.Println("update thread update timestamp")
-	createdAt := time.Unix(posts[0].LastHit, 0)
-	_, err = sq.Update("threads").
-		Set("updated_at", createdAt).
-		Where(sq.Eq{"board_id": boardID, "thread_id": threadID}).
-		PlaceholderFormat(sq.Dollar).
-		RunWith(db).
-		Exec()
-
-	if err != nil {
-		return err
-	}
-
-	log.Println("done.")
-
-	return nil
+	return upsertPosts(boardID, threadID, posts, db)
 }
 
 // syncThreads реализует интерфейс subcommands.Commander для того, чтобы
@@ -365,7 +483,16 @@ func (s *syncThreads) upsertThreads(board string, threads *api.Threads) error {
 		return err
 	}
 
-	log.Println("prepare insert or update statement")
+	if err := upsertThreads(boardID, threads, db); err != nil {
+		return err
+	}
+
+	log.Println("done.")
+
+	return nil
+}
+
+func upsertThreads(boardID int, threads *api.Threads, db *sql.DB) error {
 	stmt := sq.
 		Insert("threads").
 		Columns("thread_id", "board_id", "subject", "created_at").
@@ -378,13 +505,48 @@ func (s *syncThreads) upsertThreads(board string, threads *api.Threads) error {
 		stmt = stmt.Values(thread.Num, boardID, thread.Subject, createdAt)
 	}
 
-	log.Println("execute statement")
+	if _, err := stmt.Exec(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func upsertPosts(boardID, threadID int, posts []*api.Post, db *sql.DB) error {
+	stmt := sq.
+		Insert("posts").
+		Columns("post_id", "thread_id", "board_id",
+			"ordinal", "op",
+			"author", "email", "subject", "comment",
+			"created_at").
+		Suffix("ON CONFLICT (board_id, post_id) DO NOTHING").
+		PlaceholderFormat(sq.Dollar).
+		RunWith(db)
+
+	for _, post := range posts {
+		createdAt := time.Unix(post.Timestamp, 0)
+
+		stmt = stmt.Values(post.Num, threadID, boardID,
+			post.Number, post.Op,
+			post.Name[:128], post.Email[:128], post.Subject, post.Comment[:16384],
+			createdAt)
+	}
 
 	if _, err := stmt.Exec(); err != nil {
 		return err
 	}
 
-	log.Println("done.")
+	createdAt := time.Unix(posts[0].LastHit, 0)
+	_, err := sq.Update("threads").
+		Set("updated_at", createdAt).
+		Where(sq.Eq{"board_id": boardID, "thread_id": threadID}).
+		PlaceholderFormat(sq.Dollar).
+		RunWith(db).
+		Exec()
+
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
@@ -398,6 +560,7 @@ func main() {
 	subcommands.Register(&listPosts{}, "")
 	subcommands.Register(&listThreads{}, "")
 
+	subcommands.Register(&syncAll{}, "")
 	subcommands.Register(&syncBoards{}, "")
 	subcommands.Register(&syncPosts{}, "")
 	subcommands.Register(&syncThreads{}, "")
